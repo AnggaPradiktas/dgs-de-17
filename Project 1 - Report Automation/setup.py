a94d356b067f12fc0ee7b74cbc210910c6936efa
Angga Pradikta <apradikt@PMINTL.NET>
2024-01-02 13:48:24 +0700

    add requirements.txt

--- Project 1 - Report Automation/setup.py
+++ Project 1 - Report Automation/setup.py
@@ -2,18 +2,18 @@
 # https://godatadriven.com/blog/a-practical-guide-to-using-setup-py/
 from setuptools import setup, find_packages
 
-# with open('requirements.txt') as f:
-#     required = f.read().splitlines()
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(name='report',
       version='0.1.1',
       packages=['report','report.src', 'report.src.operators'],
       install_requires=
-      # required
-      [
-        'numpy',
-        'pandas',
-        'openpyxl',
-        'discord==1.7.3'
-    ]
+      required
+    #   [
+    #     'numpy',
+    #     'pandas',
+    #     'openpyxl',
+    #     'discord==1.7.3'
+    # ]
     )
