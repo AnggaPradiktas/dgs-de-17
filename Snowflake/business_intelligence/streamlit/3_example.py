c8f3ce6c82333282ce03c21917b27eac1db4c906
Angga Pradikta <angga@angga.com>
2024-01-15 18:54:12 +0700

    add more examples

--- Snowflake/business_intelligence/streamlit/3_example.py
+++ Snowflake/business_intelligence/streamlit/3_example.py
@@ -1,11 +1,15 @@
 # Tutorial: https://blog.streamlit.io/how-to-build-a-real-time-live-dashboard-with-streamlit/
 
 import time  # to simulate a real time data, time loop
-
+import streamlit as st
 import numpy as np  # np mean, np random
 import pandas as pd  # read csv, df manipulation
 import plotly.express as px  # interactive charts
 import streamlit as st  # 🎈 data web app development
+from snowflake.snowpark.context import get_active_session
+
+
+session = get_active_session()
 
 st.set_page_config(
     page_title="Real-Time Data Science Dashboard",
@@ -14,12 +18,12 @@ st.set_page_config(
 )
 
 # read csv from a github repo
-dataset_url = "https://raw.githubusercontent.com/Lexie88rus/bank-marketing-analysis/master/bank.csv"
+# dataset_url = "https://raw.githubusercontent.com/Lexie88rus/bank-marketing-analysis/master/bank.csv"
 
 # read csv from a URL
 @st.cache_data
 def get_data() -> pd.DataFrame:
-    return pd.read_csv(dataset_url)
+    return session.sql("SELECT * FROM HOL_DB.PUBLIC.BANK_MARKETING_ANALYSIS").to_pandas()
 
 df = get_data()
 
@@ -27,29 +31,29 @@ df = get_data()
 st.title("Real-Time / Live Data Science Dashboard")
 
 # top-level filters
-job_filter = st.selectbox("Select the Job", pd.unique(df["job"]))
+job_filter = st.selectbox("Select the Job", pd.unique(df["JOB"]))
 
 # creating a single-element container
 placeholder = st.empty()
 
 # dataframe filter
-df = df[df["job"] == job_filter]
+df = df[df["JOB"] == job_filter]
 
 # near real-time / live feed simulation
 for seconds in range(200):
 
-    df["age_new"] = df["age"] * np.random.choice(range(1, 5))
-    df["balance_new"] = df["balance"] * np.random.choice(range(1, 5))
+    df["AGE_NEW"] = df["AGE"] * np.random.choice(range(1, 5))
+    df["BALANCE_NEW"] = df["BALANCE"] * np.random.choice(range(1, 5))
 
     # creating KPIs
-    avg_age = np.mean(df["age_new"])
+    avg_age = np.mean(df["AGE_NEW"])
 
     count_married = int(
-        df[(df["marital"] == "married")]["marital"].count()
+        df[(df["MARITAL"] == "MARRIED")]["MARITAL"].count()
         + np.random.choice(range(1, 30))
     )
 
-    balance = np.mean(df["balance_new"])
+    balance = np.mean(df["BALANCE_NEW"])
 
     with placeholder.container():
 
@@ -80,13 +84,13 @@ for seconds in range(200):
         with fig_col1:
             st.markdown("### First Chart")
             fig = px.density_heatmap(
-                data_frame=df, y="age_new", x="marital"
+                data_frame=df, y="AGE_NEW", x="MARITAL"
             )
             st.write(fig)
             
         with fig_col2:
             st.markdown("### Second Chart")
-            fig2 = px.histogram(data_frame=df, x="age_new")
+            fig2 = px.histogram(data_frame=df, x="AGE_NEW")
             st.write(fig2)
 
         st.markdown("### Detailed Data View")
